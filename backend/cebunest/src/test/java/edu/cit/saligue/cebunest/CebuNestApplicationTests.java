44afefe8b468d16a65d9f3da04e6e34bd45e4f5d
LAB13_USER ON WS31 <L13Y09W31@LabHCI.net>
2026-03-07 08:25:28 +0800

    feat(SpringBoot): initilized SpringBoot project

---
+++ backend/cebunest/src/test/java/edu/cit/saligue/cebunest/CebuNestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.saligue.cebunest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CebuNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
